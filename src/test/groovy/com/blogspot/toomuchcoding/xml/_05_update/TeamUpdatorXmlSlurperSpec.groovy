d30c7d755c69eba96b5a034f4e76117813dc7d13
marcingrzejszczak <marcin@grzejszczak.pl>
2014-02-19 23:04:18 +0100

    Added example of XmlSlurper vs XmlParser

--- src/test/groovy/com/blogspot/toomuchcoding/xml/_05_update/TeamUpdatorXmlSlurperSpec.groovy
+++ src/test/groovy/com/blogspot/toomuchcoding/xml/_05_update/TeamUpdatorXmlSlurperSpec.groovy
@@ -84,4 +84,86 @@ class TeamUpdatorXmlSlurperSpec extends Specification {
         then:
             XMLUnit.compareXML(desiredXml, outputXml).similar()
     }
+
+    def "should fail to find appended node using XmlSlurper"() {
+        given:
+            String inputXml = '''
+                                <team name="FCBarcelona">
+                                    <players>
+                                        <player position="forward">
+                                            <name>Lionel</name>
+                                            <surname>Messi</surname>
+                                            <number>10</number>
+                                        </player>
+                                        <player position="forward">
+                                            <name>Neymar</name>
+                                            <surname>Neymar</surname>
+                                            <number>11</number>
+                                        </player>
+                                        <player position="midfielder">
+                                            <name>Xavi</name>
+                                            <surname>Xavi</surname>
+                                            <number>6</number>
+                                        </player>
+                                        <player position="midfielder">
+                                            <name>Andres</name>
+                                            <surname>Iniesta</surname>
+                                            <number>8</number>
+                                        </player>
+                                    </players>
+                                </team>
+                                '''
+        and:
+            def team = new XmlSlurper().parseText(inputXml)
+        and:
+            team.players.appendNode {
+                player(position: "midfielder") {
+                    name("Marcin")
+                    surname("Grzejszczak")
+                }
+            }
+        when:
+            def marcin = team.players.player.find {it.name == "Marcin"}
+        then:
+            !marcin
+    }
+
+    def "should find appended node using XmlParser"() {
+        given:
+            String inputXml = '''
+                                <team name="FCBarcelona">
+                                    <players>
+                                        <player position="forward">
+                                            <name>Lionel</name>
+                                            <surname>Messi</surname>
+                                            <number>10</number>
+                                        </player>
+                                        <player position="forward">
+                                            <name>Neymar</name>
+                                            <surname>Neymar</surname>
+                                            <number>11</number>
+                                        </player>
+                                        <player position="midfielder">
+                                            <name>Xavi</name>
+                                            <surname>Xavi</surname>
+                                            <number>6</number>
+                                        </player>
+                                        <player position="midfielder">
+                                            <name>Andres</name>
+                                            <surname>Iniesta</surname>
+                                            <number>8</number>
+                                        </player>
+                                    </players>
+                                </team>
+                                '''
+        and:
+            def team = new XmlParser().parseText(inputXml)
+            def marcin = team.players[0]?.appendNode('player', [position:"midfielder"])
+            marcin.appendNode('name', 'Marcin')
+            marcin.appendNode('surname', 'Grzejszczak')
+        when:
+            def foundMarcin = team.players.player.find {it.name[0].value() == "Marcin"}
+        then:
+            foundMarcin != null
+    }
 }
